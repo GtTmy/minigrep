9014ec6c8e7943ae487330ad8d94153a73a72728
GtTmy <tomoya.gotoh.wind@gmail.com>
2020-09-20 12:54:49 +0900

    finished the chap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    // run(config).unwrap_or_else(|err| {
+    //     println!("Application Error: {}", err);
+    //     process::exit(1);
+    // });
+    if let Err(err) = minigrep::run(config) {
+        eprintln!("Application Error: {}", err);
+        process::exit(1);
+    }
 }
+
